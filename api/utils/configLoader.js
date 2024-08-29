e1841335c4fcacc3ce397d1288a8f7ac5b860739
GuriZenit <gurizenit@gmail.com>
2024-08-29 05:24:58 -0300

    refactor: configLoader

--- api/utils/configLoader.js
+++ api/utils/configLoader.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const fs = require("fs");
 const path = require("path");
 
@@ -6,7 +8,7 @@ const PATH = path.join(__dirname, "../config/config.json");
 
 module.exports = () => {
   try {
-    const config = JSON.parse(fs.readFileSync(PATH));
+    const config = JSON.parse(fs.readFileSync(PATH, 'utf8'));
 
     cache.add(JSON.stringify(config));
 
@@ -14,8 +16,12 @@ module.exports = () => {
   } catch (error) {
     console.error("Error:", error.message, "- using cached config.json!");
 
-    const arr = cache.values().toArray();
+    const arr = Array.from(cache);
 
-    return JSON.parse(arr.toReversed()[0]);
+    if (arr.length > 0) {
+      return JSON.parse(arr[arr.length - 1]);
+    } else {
+      throw new Error("No cached config.json available.");
+    }
   }
 };
