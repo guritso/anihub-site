a3479351d759d5df1952b54f5ef138bd2cbbcb76
GuriZenit <gurizenit@gmail.com>
2024-08-31 05:42:14 -0300

    refactor: improve middlewares

--- app.js
+++ app.js
@@ -29,12 +29,18 @@ app.get("/", (req, res) => {
     console.log(`  • ${route.data.method} ${route.data.path}`);
   });
 
-  app.get("/api/*", (req, res) => {
-    res.status(404).send({ status: res.statusCode, message: "Not found" });
+  app.get("/api", (req, res) => {
+    res.send({ status: res.statusCode, message: `API is running`});
   });
 
-  app.use((req, res) => {
+  app.use((req, res, next) => {
+    if (req.path.startsWith("/api/")) return next();
     res.sendFile(path.join(__dirname, "/assets/pages/404.html"));
+  });
+  
+  app.use((req, res, next) => {
+    res.status(404).send({ status: res.statusCode, message: "Not found" });
+    next();
   });
 
   gerateKey.write({
