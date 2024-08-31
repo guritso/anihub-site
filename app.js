0916000d93a69e90ba22d4d921751e56a01c4763
GuriZenit <gurizenit@gmail.com>
2024-08-31 19:34:25 -0300

    perf: remove async funtion

--- app.js
+++ app.js
@@ -17,41 +17,39 @@ app.use(cors());
 app.locals.cache = new Map();
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
-(async () => {
-  const routes = routeMapper(path.join(__dirname, "/api/routes"));
-  console.log(`↺ loading routes...`);
+const routes = routeMapper(path.join(__dirname, "/api/routes"));
+console.log(`↺ loading routes...`);
 
-  routes.forEach((route) => {
+routes.forEach((route) => {
     app[route.data.method](route.data.path, route.handler);
     console.log(`  • ${route.data.method} ${route.data.path}`);
-  });
+});
 
-  app.get("/api", (req, res) => {
-    res.send({ status: res.statusCode, message: `API is running`});
-  });
+app.get("/api", (req, res) => {
+    res.send({ status: res.statusCode, message: `API is running` });
+});
 
-  app.use((req, res, next) => {
+app.use((req, res, next) => {
     if (req.path.startsWith("/api/")) return next();
     res.sendFile(path.join(__dirname, "/assets/pages/404.html"));
-  });
-  
-  app.use((req, res, next) => {
+});
+
+app.use((req, res, next) => {
     res.status(404).send({ status: res.statusCode, message: "Not found" });
     next();
-  });
+});
 
-  gerateKey.write({
+gerateKey.write({
     override: config().security.newKeyOnStart,
     show: config().security.showKeyOnStart
-  });
+});
 
-  console.log("↺ loading server...");
-  app.listen(config().port, () => {
+console.log("↺ loading server...");
+app.listen(config().port, () => {
     console.log("  • online on port ", config().port);
     console.log("↺ loading anime sync...");
     animeSync.start(app.locals.cache)
-  });
-})();
+});
