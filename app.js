93e3e03cc9dfa2aa97535b09d53fc35071acac78
GuriZenit <gurizenit@gmail.com>
2024-09-10 11:17:06 -0300

    feat: add rate limiting to API routes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const animeSync = require("./api/services/animeSync");
+const rateLimit = require("express-rate-limit");
 const config = require("./api/utils/configLoader");
 const routeMapper = require("./api/utils/mapper");
 const gerateKey = require("./api/utils/generateKey");
@@ -14,42 +15,55 @@ app.use("/assets", express.static(path.join(__dirname, "assets")));
 app.use(express.json());
 app.use(cors());
 
+const limiter = rateLimit({
+  windowMs: 1000,
+  max: 10,
+  handler: (req, res) => {
+    res.status(429).json({
+      status: 429,
+      message: "Too many requests"
+    });
+  }
+});
+
+app.use("/api", limiter);
+
 app.locals.cache = new Map();
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 const routes = routeMapper(path.join(__dirname, "/api/routes"));
 console.log(`↺ loading routes...`);
 
 routes.forEach((route) => {
-    app[route.data.method](route.data.path, route.handler);
-    console.log(`  • ${route.data.method} ${route.data.path}`);
+  app[route.data.method](route.data.path, route.handler);
+  console.log(`  • ${route.data.method} ${route.data.path}`);
 });
 
 app.get("/api", (req, res) => {
-    res.send({ status: res.statusCode, message: `API is running` });
+  res.send({ status: res.statusCode, message: `API is running` });
 });
 
 app.use((req, res, next) => {
-    if (req.path.startsWith("/api/")) return next();
-    res.sendFile(path.join(__dirname, "/assets/pages/404.html"));
+  if (req.path.startsWith("/api/")) return next();
+  res.sendFile(path.join(__dirname, "/assets/pages/404.html"));
 });
 
 app.use((req, res, next) => {
-    res.status(404).send({ status: res.statusCode, message: "Not found" });
-    next();
+  res.status(404).send({ status: res.statusCode, message: "Not found" });
+  next();
 });
 
 gerateKey.write({
-    override: config().security.newKeyOnStart,
-    show: config().security.showKeyOnStart
+  override: config().security.newKeyOnStart,
+  show: config().security.showKeyOnStart
 });
 
 console.log("↺ loading server...");
 app.listen(config().port, () => {
-    console.log("  • online on port ", config().port);
-    console.log("↺ loading anime sync...");
-    animeSync.start(app.locals.cache);
+  console.log("  • online on port ", config().port);
+  console.log("↺ loading anime sync...");
+  animeSync.start(app.locals.cache);
 });
