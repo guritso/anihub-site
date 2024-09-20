d1851da773eae368a7eefc249aafd6cfdfb50a2d
GuriZenit <gurizenit@gmail.com>
2024-09-19 21:01:06 -0300

    serve/pages: add 404 response status

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.get("/api", (req, res) => {
 
 app.use((req, res, next) => {
   if (req.path.startsWith("/api/")) return next();
-  return res.sendFile(path.join(process.cwd(), "/src/pages/404.html"));
+  return res.status(404).sendFile(path.join(process.cwd(), "/src/pages/404.html"));
 });
 
 app.use((req, res, next) => {
