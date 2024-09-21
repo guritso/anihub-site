7f02e8344224bcce3dcd32ecb9a2f918e9b725d9
GuriZenit <gurizenit@gmail.com>
2024-09-21 00:38:20 -0300

    utils/texter: fix not showing break lines

--- src/server/utils/texter.js
+++ src/server/utils/texter.js
@@ -1,14 +1,15 @@
 export const texter = (txt) => {
   const arr = txt.split("%H");
-  let res = txt;
+  let res = arr.join("");
 
   for (const a of arr) {
     const [n, ...w] = a.split(" ");
 
-    res = res.replace(
-      `%H${n} ${w.join(" ")}%H`,
-      `\x1b[${n}m${w.join(" ")}\x1b[0m`
-    );
+    if (!n || !txt.includes(`%H${n}`) || isNaN(n)) continue;
+
+    const mo = `\x1b[${n}m${w.join(" ")}\x1b[0m`;
+
+    res = res.replace(a, mo);
   }
 
   return res;
