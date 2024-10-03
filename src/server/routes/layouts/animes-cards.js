cef7424c837124f3fd3aa35cd0792755815dff7d
GuriZenit <gurizenit@gmail.com>
2024-10-03 03:33:12 -0300

    feat: add episodes watched in the anime card

--- src/server/routes/layouts/animes-cards.js
+++ src/server/routes/layouts/animes-cards.js
@@ -6,10 +6,11 @@ export default class AnimesCards {
 
     return  animes.filter((anime) => config.anime.filters.status.includes(anime.user.status)).map((anime, i) => i >= config.anime.limit ? "" :
     `<a class="anime-card" href="redirect?url=${encodeURIComponent(anime.link)}" target="_blank" style="background-image: url(${anime.image})">`
-      + `<p class="anime-status" id="${anime.user.status.replace(' ', '-')}">${anime.user.status}</p>`
-      + `<p id="anime-title">${anime.title}</p>`
-      + `<p id="anime-date">${anime.user.date.toTimeString().slice(0, 5)}  ${anime.user.date.toDateString()}</p>`
-      + "</a>"
+    + `<p class="anime-status" id="${anime.user.status.replace(' ', '-')}">${anime.user.status}</p>`
+    + `<p id="anime-title">${anime.title}</p>`
+    + `<p id="anime-episodes">Episodes: ${anime.user.increment} / ${anime.episodes || '?'}</p>`
+    + `<p id="anime-date">${anime.user.date.toTimeString().slice(0, 5)}  ${anime.user.date.toDateString()}</p>`
+    + "</a>"
     ).join('');
   }
 } 
